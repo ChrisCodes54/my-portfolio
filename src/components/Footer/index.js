c2c8d4e96e04e48d879e1cdd46d09df373ee4baf
Chris <72607953+ChrisCodes54@users.noreply.github.com>
2022-11-09 12:38:07 -0500

    created skeleton structure for footer

---
+++ src/components/Footer/index.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import './style.css'
+
+export default function Footer() {
+    return (
+        <footer id='footer'> 
+    
+        
+            
+                
+        <h1>hello</h1>
+           
+        
+    
+    </footer>
+
+    )
+}
