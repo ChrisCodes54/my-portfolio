2497417062acdc8489c6b57bdd54dbe2407f02bb
Chris <72607953+ChrisCodes54@users.noreply.github.com>
2022-11-10 13:46:40 -0500

    added icons, need to centered and placed, possible 3rd may be added

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,18 +1,29 @@
 import React from 'react';
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+
+import { Github } from 'react-bootstrap-icons';
+import { Linkedin } from 'react-bootstrap-icons';
+
 import './style.css'
 
 export default function Footer() {
     return (
-        <footer id='footer'> 
-    
-        
-            
-                
-        
+        <footer id='footer'>
+            <Container fluid >
+                <Row>
+                    <Col id='icon'>
+                        <Linkedin color='white' />
 
-        
-    
-    </footer>
+                        <Github color='white' />
+
+                    </Col>
+
+                </Row>
+            </Container>
+
+        </footer>
 
     )
 }
