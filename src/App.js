32b91b32cb22c54d7ff068e8df80dc0f93576f47
Chris <72607953+ChrisCodes54@users.noreply.github.com>
2022-11-23 17:32:09 -0500

    restrcuture and cleaning up file structure, re-doing the positioning for the content and setting up routes is needed for App

--- src/App.js
+++ src/App.js
@@ -1,18 +1,20 @@
-import React from 'react';
-
-import Home from '../src/pages/Home'
-import 'bootstrap/dist/css/bootstrap.min.css';
-import './App.css';
+import React from "react";
+import Header from "./components/Header";
+import About from "./pages/About";
+import Footer from "./components/Footer";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./App.css";
 
 function App() {
-  return (
-    <>
-
-      <Home />
-
-
-    </>
-  );
+	return (
+		<>
+			<Header />
+			<main>
+				<About />
+			</main>
+			<Footer />
+		</>
+	);
 }
 
 export default App;
