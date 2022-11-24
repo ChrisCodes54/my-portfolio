bc6de8e8e865653c3c507506ee4c9e205a1371c6
Chris <72607953+ChrisCodes54@users.noreply.github.com>
2022-11-24 08:23:24 -0500

    Header component does not like LinkContainer from React-Bootstrap and the entire page will refresh including header and footer, possibly need to create individual pages or figure out switches option

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
 import React from "react";
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Header from "./components/Header";
 import About from "./pages/About";
+import Projects from "./pages/Projects";
+import Contact from "./pages/Contact";
 import Footer from "./components/Footer";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
@@ -9,9 +12,22 @@ function App() {
 	return (
 		<>
 			<Header />
+
 			<main>
-				<About />
+
+				<Router>
+					<Routes>
+
+							<Route exact path='/' element={<About/>}/>
+							<Route exact path='/projects' element={<Projects/>}/>
+							<Route exact path='/contact' element={<Contact/>}/>
+
+					</Routes>
+
+				</Router>
+				
 			</main>
+
 			<Footer />
 		</>
 	);
--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,7 +1,9 @@
 import React from 'react';
+
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
+
 
 import './style.css'
 
@@ -10,13 +12,21 @@ export default function Header() {
     <>
       <Navbar collapseOnSelect expand="lg" variant="dark" id='color-nav'>
         <Container id='link-container'>
-          <Navbar.Brand href="#home" id='name-text'>Christopher Tangarife</Navbar.Brand>
+          <Navbar.Brand href="/" id='name-text'>Christopher Tangarife</Navbar.Brand>
           <Navbar.Toggle aria-controls="responsive-navbar-nav"  />
           <Navbar.Collapse id="responsive-navbar-nav" >
             <Nav className="me-auto" id='font-size'>
-              <Nav.Link href="#aboutme" id='links'>About Me</Nav.Link>
-              <Nav.Link href="#projects" id='links'>Projects</Nav.Link>
-              <Nav.Link href="#contact" id='links'>Contact</Nav.Link>
+
+            
+              <Nav.Link href="/" id='links'>About Me</Nav.Link>
+
+
+              <Nav.Link href="/projects" id='links'>Projects</Nav.Link>
+
+
+              <Nav.Link href="/contact" id='links'>Contact</Nav.Link>
+
+
             </Nav>
           </Navbar.Collapse>
         </Container>
