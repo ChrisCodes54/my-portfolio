95c560252a47414b70d4ec17e7548d6911f173d5
Chris <72607953+ChrisCodes54@users.noreply.github.com>
2022-12-21 11:34:10 -0500

    layout changes to about me section

--- src/pages/About/index.js
+++ src/pages/About/index.js
@@ -9,7 +9,9 @@ import './style.css'
 export default function About() {
 	return (
 		<main id="main-aboutme">
+
 			<h1 id="aboutme-header">About Me</h1>
+			
 		<Transitions>
 			<Container fluid id='aboutme-container'>
 				
@@ -19,6 +21,29 @@ export default function About() {
 					</Col>
 					<Col id='aboutmetext-column'>
 						Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+						Aliquam mattis leo gravida, dapibus lorem non, facilisis
+						ligula. Donec malesuada nunc nec consectetur varius. Fusce
+						rhoncus ultrices leo et eleifend. Cras nunc ligula,
+						imperdiet sed ipsum sed, fringilla accumsan ante. Praesent
+						porta ante varius, tincidunt enim hendrerit, ornare leo.
+						Cras sodales diam nibh, vel tempus dolor egestas id.
+						Curabitur sem erat, porttitor quis nibh in, porta molestie
+						nibh. Vestibulum auctor ipsum lacus, vitae fermentum enim
+						aliquam a. Duis aliquet ac orci sed mattis. Cras nec elit
+						vitae eros viverra elementum. Nam vestibulum quam a nisi
+						imperdiet porttitor. Praesent suscipit ac mi eget egestas.
+						Curabitur eget mollis orci, convallis condimentum nisi.
+						Etiam efficitur, diam id iaculis fermentum, metus dolor
+						ultricies metus, maximus porta magna mauris non felis. Orci
+						varius natoque penatibus et magnis dis parturient montes,
+						nascetur ridiculus mus.Duis aliquet ac orci sed mattis. Cras
+						nec elit vitae eros viverra elementum. Nam vestibulum quam a
+						nisi imperdiet porttitor. Praesent suscipit ac mi eget
+						egestas. Curabitur eget mollis orci, convallis condimentum
+						nisi. Etiam efficitur, diam id iaculis fermentum, metus
+						dolor ultricies metus, maximus porta magna mauris non felis.
+						Orci varius natoque penatibus et magnis dis parturient
+						montes, nascetur ridiculus mus.Lorem ipsum dolor sit amet, consectetur adipiscing elit.
 						Aliquam mattis leo gravida, dapibus lorem non, facilisis
 						ligula. Donec malesuada nunc nec consectetur varius. Fusce
 						rhoncus ultrices leo et eleifend. Cras nunc ligula,
