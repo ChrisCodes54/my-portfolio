1337d07c181fdf4591980d1812a5d3f2fcd65bbe
Chris <72607953+ChrisCodes54@users.noreply.github.com>
2022-12-23 07:33:30 -0500

    changed color combination layout

--- src/pages/About/index.js
+++ src/pages/About/index.js
@@ -8,72 +8,24 @@ import './style.css'
 
 export default function About() {
 	return (
-		<>
+		<main id="main-aboutme">
 
 			<h1 id="aboutme-header">About Me</h1>
-			<section id="giggles">
 
-				<Transitions>
-					<Container fluid id='aboutme-container'>
+		<Transitions>
+			<Container fluid id='aboutme-container'>
+				
+				<Row>
+					<Col id="picture-column">
+						<img src={pc} alt="Christopher Tangarife" id="profile-picture" />
+					</Col>
+					<Col id='aboutmetext-column'>
+						Hello and welcome to my Portfolio! My name is Christopher Tangarife and I am a Full Stack Developer Bootcamp Graduate. I am a New Jersey native born and raised, since I was a kid I was always interested in the small details of how things works and why they work. I enjoyed building different types of things from as small as Legos to computers and this has followed me into my adulthood. What one of the greatest gifts we all have is the ability to learn and to adpat, this is what gets me out of bed everyday. That there is always something new to learn or more personally there is always a bug that can be fixed. I have an unexplainable drive to constantly push myself to be better, to push forward, and when possible to help others.
+					</Col>
+				</Row>
+			</Container>
 
-						<Row>
-							<Col id="picture-column">
-								<img src={pc} alt="Christopher Tangarife" id="profile-picture" />
-							</Col>
-							<Col id='aboutmetext-column'>
-								Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-								Aliquam mattis leo gravida, dapibus lorem non, facilisis
-								ligula. Donec malesuada nunc nec consectetur varius. Fusce
-								rhoncus ultrices leo et eleifend. Cras nunc ligula,
-								imperdiet sed ipsum sed, fringilla accumsan ante. Praesent
-								porta ante varius, tincidunt enim hendrerit, ornare leo.
-								Cras sodales diam nibh, vel tempus dolor egestas id.
-								Curabitur sem erat, porttitor quis nibh in, porta molestie
-								nibh. Vestibulum auctor ipsum lacus, vitae fermentum enim
-								aliquam a. Duis aliquet ac orci sed mattis. Cras nec elit
-								vitae eros viverra elementum. Nam vestibulum quam a nisi
-								imperdiet porttitor. Praesent suscipit ac mi eget egestas.
-								Curabitur eget mollis orci, convallis condimentum nisi.
-								Etiam efficitur, diam id iaculis fermentum, metus dolor
-								ultricies metus, maximus porta magna mauris non felis. Orci
-								varius natoque penatibus et magnis dis parturient montes,
-								nascetur ridiculus mus.Duis aliquet ac orci sed mattis. Cras
-								nec elit vitae eros viverra elementum. Nam vestibulum quam a
-								nisi imperdiet porttitor. Praesent suscipit ac mi eget
-								egestas. Curabitur eget mollis orci, convallis condimentum
-								nisi. Etiam efficitur, diam id iaculis fermentum, metus
-								dolor ultricies metus, maximus porta magna mauris non felis.
-								Orci varius natoque penatibus et magnis dis parturient
-								montes, nascetur ridiculus mus.Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-								Aliquam mattis leo gravida, dapibus lorem non, facilisis
-								ligula. Donec malesuada nunc nec consectetur varius. Fusce
-								rhoncus ultrices leo et eleifend. Cras nunc ligula,
-								imperdiet sed ipsum sed, fringilla accumsan ante. Praesent
-								porta ante varius, tincidunt enim hendrerit, ornare leo.
-								Cras sodales diam nibh, vel tempus dolor egestas id.
-								Curabitur sem erat, porttitor quis nibh in, porta molestie
-								nibh. Vestibulum auctor ipsum lacus, vitae fermentum enim
-								aliquam a. Duis aliquet ac orci sed mattis. Cras nec elit
-								vitae eros viverra elementum. Nam vestibulum quam a nisi
-								imperdiet porttitor. Praesent suscipit ac mi eget egestas.
-								Curabitur eget mollis orci, convallis condimentum nisi.
-								Etiam efficitur, diam id iaculis fermentum, metus dolor
-								ultricies metus, maximus porta magna mauris non felis. Orci
-								varius natoque penatibus et magnis dis parturient montes,
-								nascetur ridiculus mus.Duis aliquet ac orci sed mattis. Cras
-								nec elit vitae eros viverra elementum. Nam vestibulum quam a
-								nisi imperdiet porttitor. Praesent suscipit ac mi eget
-								egestas. Curabitur eget mollis orci, convallis condimentum
-								nisi. Etiam efficitur, diam id iaculis fermentum, metus
-								dolor ultricies metus, maximus porta magna mauris non felis.
-								Orci varius natoque penatibus et magnis dis parturient
-								montes, nascetur ridiculus mus.
-							</Col>
-						</Row>
-					</Container>
-
-				</Transitions>
-			</section>
-		</>
+		</Transitions>
+</main>
 	)
 }
