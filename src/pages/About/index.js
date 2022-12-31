1f252ddd74e50a9b6e787f4dcd33e0af913d5099
Chris <72607953+ChrisCodes54@users.noreply.github.com>
2022-12-31 08:25:33 -0500

    finalizing layout structure for about me section, content text needs to be added, and then responiveness mobile layout

--- src/pages/About/index.js
+++ src/pages/About/index.js
@@ -12,20 +12,20 @@ export default function About() {
 
 			<h1 id="aboutme-header">About Me</h1>
 
-		<Transitions>
-			<Container fluid id='aboutme-container'>
-				
-				<Row>
-					<Col id="picture-column">
-						<img src={pc} alt="Christopher Tangarife" id="profile-picture" />
-					</Col>
-					<Col id='aboutmetext-column'>
-						Hello and welcome to my Portfolio! My name is Christopher Tangarife and I am a Full Stack Developer Bootcamp Graduate. I am a New Jersey native born and raised, since I was a kid I was always interested in the small details of how things works and why they work. I enjoyed building different types of things from as small as Legos to computers and this has followed me into my adulthood. What one of the greatest gifts we all have is the ability to learn and to adpat, this is what gets me out of bed everyday. That there is always something new to learn or more personally there is always a bug that can be fixed. I have an unexplainable drive to constantly push myself to be better, to push forward, and when possible to help others.
-					</Col>
-				</Row>
-			</Container>
+			<Transitions>
+				<Container fluid id='aboutme-container'>
 
-		</Transitions>
-</main>
+					<Row id="rows">
+						<Col id="picture-column">
+							<img src={pc} alt="Christopher Tangarife" id="profile-picture" />
+						</Col>
+						<Col id='aboutme-column'>
+							<p id="aboutme-text">Hello and welcome to my Portfolio! My name is Christopher Tangarife and I am a Full Stack Developer Bootcamp Graduate. I am a New Jersey native born and raised, since I was a kid I was always interested in the small details of how things works and why they work. I enjoyed building different types of things from as small as Legos to computers and this has followed me into my adulthood. What one of the greatest gifts we all have is the ability to learn and to adpat, this is what gets me out of bed everyday. That there is always something new to learn or more personally there is always a bug that can be fixed. I have an unexplainable drive to constantly push myself to be better, to push forward, and when possible to help others.</p>
+						</Col>
+					</Row>
+				</Container>
+
+			</Transitions>
+		</main>
 	)
 }
