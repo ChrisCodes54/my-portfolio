9122052cb8b26472a75649fd0955674ac3dbf7ad
Chris <72607953+ChrisCodes54@users.noreply.github.com>
2023-03-05 11:54:24 -0500

    cards are nearly finalized with their styling, ,mobile responsiveness needs to be added. Then once that is fully complete content can be added to it

--- src/pages/Projects/index.js
+++ src/pages/Projects/index.js
@@ -6,55 +6,34 @@ import './style.css'
 
 export default function Projects() {
   const cardData = [
-    
-      { id: 1, Img: '"https://via.placeholder.com/150"', title: 'This is card 1', text: "" },
-          { id: 2, Img: "https://via.placeholder.com/150", title: 'This is card 2' },
-           { id: 3, Img: "https://via.placeholder.com/150", title: 'This is card 3' },
-    
+
+    { id: 1, Img: '"https://via.placeholder.com/150"', title: 'Tech Blog', text: "" },
+    { id: 2, Img: "https://via.placeholder.com/150", title: 'This is card 2' },
+    { id: 3, Img: "https://via.placeholder.com/150", title: 'This is card 3' },
+
   ]
-    return (
-      <div id="projects">
+  return (
+    <div id="projects">
       <Transitions>
-     <h1 id="projectheader">This is a projects page</h1>
+        <h1 id="projectheader">This is a projects page</h1>
 
-     {cardData.map((card) => (
-     <Card key={card.id} border="danger" style={{ width: '18rem' }}>
-      <Card.Img variant="top" src="https://via.placeholder.com/150" />
-      <Card.Body>
-        <Card.Title>{card.title}</Card.Title>
-        <Card.Text>
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
-        </Card.Text>
-        <Button variant="primary">Go somewhere</Button>
-        <Button variant="danger">Go somewhere</Button>
-      </Card.Body>
-    </Card>
-    ))}
+        <div className="card-container">
+          {cardData.map((card) => (
+            <Card key={card.id} border="danger" style={{ width: '25rem'}} className="card">
+              <Card.Img variant="top" src="https://via.placeholder.com/150" />
+              <Card.Body>
+                <Card.Title>{card.title}</Card.Title>
+                <Card.Text>
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                </Card.Text>
+                <Button variant="primary">Go somewhere</Button>
+                <Button variant="danger">Go somewhere</Button>
+              </Card.Body>
+            </Card>
+          ))}
+        </div>
+
       </Transitions>
-     </div>
-    )
-  }
-
-//   import { Card } from 'react-bootstrap';
-
-  
-// function MyCards(props) {
-//   const cardData = [
-//     { id: 1, title: 'Card 1', text: 'This is card 1' },
-//     { id: 2, title: 'Card 2', text: 'This is card 2' },
-//     { id: 3, title: 'Card 3', text: 'This is card 3' },
-//   ];
-
-//   return (
-//     <div>
-//       {cardData.map((card) => (
-//         <Card key={card.id}>
-//           <Card.Header>{card.title}</Card.Header>
-//           <Card.Body>
-//             <Card.Text>{card.text}</Card.Text>
-//           </Card.Body>
-//         </Card>
-//       ))}
-//     </div>
-//   );
-// }
+    </div>
+  )
+}
