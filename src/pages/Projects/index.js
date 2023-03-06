b3bc7ac1b6ca826dc1fa78fcc1fe8a026e2f7aff
Chris <72607953+ChrisCodes54@users.noreply.github.com>
2023-03-06 13:15:50 -0500

    added starter content to the cards to test out button redirection with repo and deployment, mobile responsiveness still needed

--- src/pages/Projects/index.js
+++ src/pages/Projects/index.js
@@ -7,27 +7,27 @@ import './style.css'
 export default function Projects() {
   const cardData = [
 
-    { id: 1, Img: '"https://via.placeholder.com/150"', title: 'Tech Blog', text: "" },
-    { id: 2, Img: "https://via.placeholder.com/150", title: 'This is card 2' },
-    { id: 3, Img: "https://via.placeholder.com/150", title: 'This is card 3' },
+    { id: 1, Img: '"https://via.placeholder.com/150"', title: 'Note Taker', text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.", repo:'https://github.com/ChrisCodes54/note-taker', deployment:"" },
+
+    { id: 2, Img: "https://via.placeholder.com/150", title: 'Day Planner', text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.", repo:'https://github.com/ChrisCodes54/Day-Planner', deployment:""  },
+
+    { id: 3, Img: "https://via.placeholder.com/150", title: 'This is card 3', text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua." },
 
   ]
   return (
     <div id="projects">
       <Transitions>
-        <h1 id="projectheader">This is a projects page</h1>
+        <h1 id="projectheader">Projects</h1>
 
         <div className="card-container">
           {cardData.map((card) => (
-            <Card key={card.id} border="danger" style={{ width: '25rem'}} className="card">
+            <Card key={card.id} border="danger" style={{ width: '25rem' }} className="card">
               <Card.Img variant="top" src="https://via.placeholder.com/150" />
-              <Card.Body>
+              <Card.Body id="card-body">
                 <Card.Title>{card.title}</Card.Title>
-                <Card.Text>
-                  Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
-                </Card.Text>
-                <Button variant="primary">Go somewhere</Button>
-                <Button variant="danger">Go somewhere</Button>
+                <Card.Text>{card.text}</Card.Text>
+                <Button href={card.repo} target="_blank" id="card-buttons" variant="primary">Repo</Button>
+                <Button id="card-buttons" variant="danger">Deployment</Button>
               </Card.Body>
             </Card>
           ))}
