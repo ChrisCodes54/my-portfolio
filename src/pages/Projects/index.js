9abf553dfc6dc13056b14cd7f93b126ec9fa3fde
Chris <72607953+ChrisCodes54@users.noreply.github.com>
2023-03-10 13:18:42 -0500

    added pictures to cards, but text for each card needs to be added along with the 3rd card needing to be done

--- src/pages/Projects/index.js
+++ src/pages/Projects/index.js
@@ -1,42 +1,46 @@
 import React from "react";
 import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
 import Button from 'react-bootstrap/Button';
 import Card from 'react-bootstrap/Card';
+import dayplanner from '../../images/usingdayplanner.png';
+import notetaker from '../../images/notetaker.png'
 import Transitions from "../transitions";
 import './style.css'
 
 export default function Projects() {
   const cardData = [
 
-    { id: 1, Img: '"https://via.placeholder.com/150"', title: 'Note Taker', text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.", repo:'https://github.com/ChrisCodes54/note-taker', deployment:"https://main.d2b9oyavnstb8q.amplifyapp.com/" },
+    { id: 1, img: notetaker, title: 'Note Taker', text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.", repo: 'https://github.com/ChrisCodes54/note-taker'},
 
-    { id: 2, Img: "https://via.placeholder.com/150", title: 'Day Planner', text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.", repo:'https://github.com/ChrisCodes54/Day-Planner', deployment:""  },
+    { id: 2, img: dayplanner, title: 'Day Planner', text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.", repo: 'https://github.com/ChrisCodes54/Day-Planner' },
 
-    { id: 3, Img: "https://via.placeholder.com/150", title: 'This is card 3', text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua." },
+    { id: 3, img: "https://placehold.jp/3d4070/ffffff/200x150.png", title: 'Tech-Blog', text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.", repo:'https://github.com/ChrisCodes54/tech-blog' },
 
   ]
   return (
     <Container fluid>
-    <div id="projects">
-      <Transitions>
-        <h1 id="projectheader">Projects</h1>
+      <div id="projects">
+        <Transitions>
+          <h1 id="projectheader">Projects</h1>
 
-        <div className="card-container">
-          {cardData.map((card) => (
-            <Card key={card.id} border="danger" style={{ width: '25rem' }} className="card">
-              <Card.Img variant="top" src="https://via.placeholder.com/150" />
-              <Card.Body id="card-body">
-                <Card.Title>{card.title}</Card.Title>
-                <Card.Text>{card.text}</Card.Text>
-                <Button href={card.repo} target="_blank" id="card-buttons" variant="primary">Repo</Button>
-                <Button href={card.deployment} target="_blank" id="card-buttons" variant="danger">Deployment</Button>
-              </Card.Body>
-            </Card>
-          ))}
-        </div>
+          <Row md={12}>
 
-      </Transitions>
-    </div>
+            <div className="card-container">
+              {cardData.map((card) => (
+                <Card key={card.id} border="dark" style={{ width: '25rem' }} className="card">
+                  <Card.Img variant="top" src={card.img} alt='project screenshot' className="cardImage"/>
+                  <Card.Body id="card-body">
+                    <Card.Title>{card.title}</Card.Title>
+                    <Card.Text>{card.text}</Card.Text>
+                    <Button href={card.repo} target="_blank" id="card-buttons" variant="primary">Repo</Button>
+                  </Card.Body>
+                </Card>
+              ))}
+            </div>
+          </Row>
+        </Transitions>
+      </div>
     </Container>
   )
 }
