01f7c928d56c2693284e570d80b5a6916794fcbb
Chris <72607953+ChrisCodes54@users.noreply.github.com>
2023-03-18 19:53:26 -0400

    set up emailjs in contact, tested functionality currently works, form does clear after submission needs to be tested more thoroughly. Stlying of the form still needs to be done

--- src/pages/Contact/index.js
+++ src/pages/Contact/index.js
@@ -1,13 +1,36 @@
-import React from "react";
+import React, { useRef } from 'react';
+import emailjs from '@emailjs/browser';
 import Transitions from "../transitions";
 import './style.css'
 
 export default function Contact() {
-    return (
-      <div id="contact">
+  const form = useRef();
+
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs.sendForm('service_d0eiqek', 'template_6ddebja', form.current, 'user_e3xioD9DxjxuKMVKwRYSc')
+      .then((result) => {
+        console.log(result.text);
+        form.current.reset();
+      }, (error) => {
+        console.log(error.text);
+      });
+  };
+  return (
+    <div id="contact">
       <Transitions>
-     <h1>This is a contact page</h1>
+        <h1 id='contact-header'>Contact Me</h1>
+        <form ref={form} onSubmit={sendEmail}>
+          <label>Name</label>
+          <input type="text" name="user_name" />
+          <label>Email</label>
+          <input type="email" name="user_email" />
+          <label>Message</label>
+          <textarea name="message" />
+          <input type="submit" value="Send" />
+        </form>
       </Transitions>
-     </div>
-    )
-  }
+    </div>
+  )
+}
