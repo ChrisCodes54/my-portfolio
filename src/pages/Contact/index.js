163904626bfaa9e2a00cc0123e83bdb853c1c0f4
Chris <72607953+ChrisCodes54@users.noreply.github.com>
2023-03-19 11:45:46 -0400

    stlyistically improved, is completely functional after testing, styles still needs to be finalized

--- src/pages/Contact/index.js
+++ src/pages/Contact/index.js
@@ -18,18 +18,21 @@ export default function Contact() {
       });
   };
   return (
-    <div id="contact">
+    <div id="contact-section">
+      
       <Transitions>
         <h1 id='contact-header'>Contact Me</h1>
-        <form ref={form} onSubmit={sendEmail}>
-          <label>Name</label>
+        <div>
+        <form id='form-box' ref={form} onSubmit={sendEmail}>
+          <label id='label'>Name</label>
           <input type="text" name="user_name" />
-          <label>Email</label>
+          <label id='label'>Email</label>
           <input type="email" name="user_email" />
-          <label>Message</label>
-          <textarea name="message" />
+          <label id='label' >Message</label>
+          <textarea id='message' name="message" />
           <input type="submit" value="Send" />
         </form>
+        </div>
       </Transitions>
     </div>
   )
